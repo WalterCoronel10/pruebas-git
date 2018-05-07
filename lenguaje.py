f9343ab3704e42c7e215831af6283bcfc9d4646a
Dylan230 <dylanalbertojossasi@gmail.com>
2018-05-07 13:54:42 -0300

    Calcula suma, resta, multiplicacion, division y resto

--- lenguaje.py
+++ lenguaje.py
@@ -10,6 +10,8 @@ res3 = num1 * num2
 
 res4 = num1 / num2 
 
+res5 = num1 % num2
+
 print "El resultado de la suma es: {}".format(res)
 
 print "El resultado de la resta es: {}".format(res2)
@@ -17,3 +19,6 @@ print "El resultado de la resta es: {}".format(res2)
 print "El resultado de la multiplicacion es: {}".format(res3)
 
 print "El resultado de la division es: {}".format(res4)
+
+print "El resto es: {}".format(res5)
+
