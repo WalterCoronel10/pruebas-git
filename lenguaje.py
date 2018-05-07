9b12aa65e36d85dd1eb34250acceb92a9e262c44
WalterCoronel10 <waltercoronel1052000@gmail.com>
2018-05-07 11:58:32 -0300

    calcula suma y resta

---
+++ lenguaje.py
@@ -0,0 +1,11 @@
+num1 = input("Ingrese numero")
+
+num2 = input("Ingrese otro numero")
+
+res = num1 + num2 
+
+res2 = num1 - num2 
+
+print "El resultado de la suma es: {}".format(res)
+
+print "El resultado de la resta es: {}".format(res2)
